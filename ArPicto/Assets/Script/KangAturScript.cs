53ebfc22ef678659b6b9725810753f18b2dcfa27
Kafura <robbykafura@gmail.com>
2025-05-22 12:27:49 +0700

    First push

---
+++ ArPicto/Assets/Script/KangAturScript.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class KangAturScript : MonoBehaviour
+{
+    public TrackableAR[] tr;
+    public string[] nama;
+    [TextArea]
+    public string[] deskripsi;
+    
+    [Header("UI Deskripsi")]
+    public TMP_Text txtNama;
+    public TMP_Text txtDeskripsi;
+
+    public GameObject goNama;
+    public GameObject goDeskripsi;
+    public GameObject Penanda;
+
+    public bool[] cekMarker;
+    int CountMarker;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cekMarker = new bool[tr.Length];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0 ; i<tr.Length; i++)
+        {
+            if (tr[i].GetMarker())
+            {
+                txtNama.text = nama[i];
+                txtDeskripsi.text = deskripsi[i];
+
+                if(!cekMarker[i])
+                {
+                    CountMarker++;
+                    cekMarker[i] = true;
+                }
+            }
+            else 
+            {
+                if (cekMarker[i])
+                {
+                    CountMarker--;
+                    cekMarker[i] = false;
+                }
+            }
+        }
+        DeskripsiPanel();
+    }
+
+    private void DeskripsiPanel()
+    {
+        if(CountMarker == 0 )
+        {
+            goNama.SetActive(false);
+            goDeskripsi.SetActive(false);
+
+            Penanda.SetActive(true);
+        }
+        else{
+            goNama.SetActive(true);
+            goDeskripsi.SetActive(true);
+
+            Penanda.SetActive(false);
+        }
+    }
+}
---
+++ ArPicto/Assets/Script/homepage.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class homepage : MonoBehaviour
+{
+
+    public GameObject[] UI1;
+    public GameObject[] UI2;
+    public GameObject[] UI3;
+
+    private int index;
+    
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    
+}
