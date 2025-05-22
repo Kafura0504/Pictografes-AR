6bfec240e6a9976add3dc6de6e9a4f80ff75a777
Kafura <robbykafura@gmail.com>
2025-05-22 13:02:04 +0700

    anim

--- ArPicto/Assets/Script/homepage.cs
+++ ArPicto/Assets/Script/homepage.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class homepage : MonoBehaviour
@@ -6,9 +7,10 @@ public class homepage : MonoBehaviour
     public GameObject[] UI1;
     public GameObject[] UI2;
     public GameObject[] UI3;
+    public GameObject[] bg;
 
     private int index;
-    
+
 
     void Start()
     {
@@ -19,5 +21,29 @@ public class homepage : MonoBehaviour
     {
 
     }
+
+    public IEnumerator firstpage()
+    {
+        for (int i = 0; i < UI1.Length; i++)
+        {
+            Animator anim = UI1[i].GetComponent<Animator>();
+            UI1[i].SetActive(true);
+            anim.Play(UI1[i].name + "in");
+            yield return new WaitForSeconds(0.5f);
+        }
+
+    }
+
+    public IEnumerator baground()
+    {
+        for (int i = 0; i < bg.Length; i++)
+        {
+            Animator anim = bg[i].GetComponent<Animator>();
+            bg[i].SetActive(true);
+            anim.Play(bg[i].name + "in");
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    
     
 }
