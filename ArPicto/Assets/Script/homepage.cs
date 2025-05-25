428d1a660c33d05c561085bc868463c36a089187
Kafura <robbykafura@gmail.com>
2025-05-25 14:47:24 +0700

    UI MARI

--- ArPicto/Assets/Script/homepage.cs
+++ ArPicto/Assets/Script/homepage.cs
@@ -11,6 +11,8 @@ public class homepage : MonoBehaviour
     public GameObject[] bg;
     public GameObject hal1;
     public GameObject hal2;
+    public GameObject sec1;
+    public GameObject sec2;
 
 
     private int index = 0;
@@ -32,6 +34,16 @@ public class homepage : MonoBehaviour
         {
             hal1.SetActive(false);
             hal2.SetActive(true);
+            sec1.SetActive(true);
+            sec2.SetActive(false);
+            
+        }
+        else if (index == 2)
+        {
+            hal1.SetActive(false);
+            hal2.SetActive(true);
+            sec1.SetActive(false);
+            sec2.SetActive(true);
         }
         else if (index == 3)
         {
